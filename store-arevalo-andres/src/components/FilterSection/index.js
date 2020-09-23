ad0706e79c43ab61324c15e78d3bf3f741603fad
Andres Arevalo <aarevalo@smartgridcis.com>
2020-09-23 00:23:15 -0500

    Initial components structure

---
+++ store-arevalo-andres/src/components/FilterSection/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FilterSection = () => {
+    return (
+        <div>
+            FilterSection
+        </div>
+    );
+};
---
+++ store-arevalo-andres/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HeaderUser }  from '../components/HeaderUser/index'
+import { Header }  from '../components/Header/index'
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <HeaderUser/>
+        </div>
+    );
+};
+
+export default Home;
