9961004851db24596da6766f56963b93a9df37b0
Andres Arevalo <andres.arevalopar@gmail.com>
2020-09-30 10:04:47 -0500

    Updates

---
+++ precache-manifest.33d203e019006d3d4f885453b8ad570c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181298edbd17e92b3bce7062a387a3be",
+    "url": "/rewardStore/index.html"
+  },
+  {
+    "revision": "f8ff7874b5f74f32d276",
+    "url": "/rewardStore/static/css/2.d25bf5f9.chunk.css"
+  },
+  {
+    "revision": "07f92d522181b9ffe491",
+    "url": "/rewardStore/static/css/main.7fbd9e58.chunk.css"
+  },
+  {
+    "revision": "f8ff7874b5f74f32d276",
+    "url": "/rewardStore/static/js/2.3b0372c9.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/rewardStore/static/js/2.3b0372c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f92d522181b9ffe491",
+    "url": "/rewardStore/static/js/main.16e42ab0.chunk.js"
+  },
+  {
+    "revision": "bccd8cb0838b116cf496",
+    "url": "/rewardStore/static/js/runtime-main.cc97fddd.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/rewardStore/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/rewardStore/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/rewardStore/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/rewardStore/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/rewardStore/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/rewardStore/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a343e15346be343af7867fa8f95ad9d5",
+    "url": "/rewardStore/static/media/header.a343e153.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/rewardStore/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/rewardStore/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/rewardStore/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/rewardStore/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/rewardStore/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/rewardStore/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/rewardStore/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/rewardStore/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/rewardStore/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/rewardStore/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
